a58c72900173ee9a938ec36186c887ef3ac05718
jcao <mail@jcaonet.com>
2026-02-06 15:55:27 +0900

    fix: 代码合并出错的修正

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -18,37 +18,21 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-<<<<<<< HEAD
-			name: "Bilibli",
-			icon: "fa7-brands:bilibili",
-			url: "https://space.bilibili.com/38932988",
-=======
 			name: "Steam",
 			icon: "fa6-brands:steam",
 			url: "https://steamcommunity.com/id/GarfieldFish/",
->>>>>>> e994ea3 (feat: 个性化修正，改一些菜单名字)
 			showName: false,
 		},
 		{
 			name: "GitHub",
-<<<<<<< HEAD
-			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
-=======
 			icon: "fa6-brands:github",
 			url: "https://github.com/KJunC",
->>>>>>> e994ea3 (feat: 个性化修正，改一些菜单名字)
 			showName: false,
 		},
 		{
 			name: "Email",
-<<<<<<< HEAD
-			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
-=======
 			icon: "fa6-solid:envelope",
 			url: "mailto:mail@jcaonet.com",
->>>>>>> e994ea3 (feat: 个性化修正，改一些菜单名字)
 			showName: false,
 		},
 		{
